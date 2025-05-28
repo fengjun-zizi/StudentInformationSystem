afeefca25162a1466225d85a60de0f3350313386
李沣峻 <lifengjun@lifengjundeMac-mini.local>
2025-05-28 14:57:21 +0700

    Initial commit

---
+++ src/main/java/com/mycompany/studentinformationsystem/StudentInformationSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.studentinformationsystem;
+
+/**
+ *
+ * @author lifengjun
+ */
+public class StudentInformationSystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
